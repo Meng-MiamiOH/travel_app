4fc1f65bb0347cdec442e1172b3eb2f7688ee97b
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-11-12 12:25:58 -0500

    all functions

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,19 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
+    >
       {icon && <Image src={icon} alt={title} height={24} width={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   );
 };
